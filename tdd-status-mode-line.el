9b3c35b0a972772640e9fee653eab6a76e06416a
Gergely Nagy <algernon@madhouse-project.org>
2013-11-23 18:16:53 +0100

    Bump the version to 0.1.2
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- tdd-status-mode-line.el
+++ tdd-status-mode-line.el
@@ -4,7 +4,7 @@
 
 ;; Author: Gergely Nagy <algernon@madhouse-project.org>
 ;; URL: https://github.com/algernon/tdd-status-mode-line
-;; Version: 0.1.1
+;; Version: 0.1.2
 ;; Keywords: faces tdd
 ;; Prefix: tdd-status
 ;; Separator: /
